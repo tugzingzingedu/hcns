0778a1db8eeebdffbfad283d13f90fc586277a41
tugzingzingedu <tugzingzing@thkd.edu.vn>
2026-02-03 16:17:08 +0700

    Add commands to manage network drive and credentials

--- loader.ps1
+++ loader.ps1
@@ -1,1 +1,9 @@
+cmdkey /delete:10.4.6.252
+cmdkey /add:10.4.6.252 /user:hcnsstaff /pass:Admin@123
+
+Start-Process explorer "\\10.4.6.252\HR"
+
+net use H: "\\10.4.6.252\HR" /persistent:yes
+
+Start-Process explorer "\\10.4.6.252\HR"
 
