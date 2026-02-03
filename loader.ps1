a74a45d11ad492e9865a1ef23260540ef4bfa070
tugzingzingedu <tugzingzing@thkd.edu.vn>
2026-02-03 16:45:29 +0700

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,38 +1,17 @@
-# =========================
-# CẤU HÌNH
-# =========================
 $target = "10.4.6.252"
 $user   = "hcnsstaff"
 $pass   = "Admin@123"
 $share  = "\\10.4.6.252\HR"
 $drive  = "H"
 
-# =========================
-# XOÁ CREDENTIAL CŨ (NẾU CÓ)
-# =========================
-$credExists = cmdkey /list | Select-String $target
-if ($credExists) {
+if (cmdkey /list | Select-String $target) {
     cmdkey /delete:$target
 }
 
-# =========================
-# THÊM CREDENTIAL MỚI
-# =========================
-cmdkey /add:$target /user:$user /pass:$pass
-
-# =========================
-# XOÁ Ổ MẠNG CŨ (NẾU CÓ)
-# =========================
 if (Get-PSDrive -Name $drive -ErrorAction SilentlyContinue) {
     Remove-PSDrive -Name $drive -Force
 }
 
-# =========================
-# MAP Ổ MẠNG
-# =========================
+cmdkey /add:$target /user:$user /pass:$pass
 New-PSDrive -Name $drive -PSProvider FileSystem -Root $share -Persist
-
-# =========================
-# MỞ EXPLORER
-# =========================
 Start-Process explorer "$drive:\"
